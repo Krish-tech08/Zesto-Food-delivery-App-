d6b1a7e67694a5a2ad3a0f003d7c373e267ab254
Krish-tech08 <krischaurasia890@gmail.com>
2025-04-22 18:57:07 +0530

    first check

--- app/src/main/java/com/example/foodapp/locationchoose.kt
+++ app/src/main/java/com/example/foodapp/locationchoose.kt
@@ -1,25 +1,25 @@
 package com.example.foodapp
-import android.widget.ArrayAdapter
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.foodapp.databinding.ActivityLocationChooseBinding
 import com.example.foodapp.databinding.ActivityStartScreenBinding
 
-class location_choose : AppCompatActivity() {
-    private  val binding: ActivityStartScreenBinding by lazy {
-        ActivityStartScreenBinding.inflate(layoutInflater)
+class locationchoose : AppCompatActivity() {
+    private  val binding: ActivityLocationChooseBinding by lazy {
+        ActivityLocationChooseBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-//val locationList= arrayOf("Delhi", "Mumbai", "Bengaluru", "Hyderabad", "Chennai", "Kolkata", "Pune", "Ahmedabad", "Jaipur", "Surat",
-//    "Lucknow", "Kanpur", "Nagpur", "Indore", "Bhopal", "Patna", "Ludhiana", "Agra", "Vadodara", "Nashik",
-//    "Faridabad", "Meerut", "Rajkot", "Amritsar", "Varanasi", "Ranchi", "Guwahati", "Chandigarh", "Coimbatore", "Thiruvananthapuram")
-//        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, locationList)
-//         binding.listOf_location.setAdapter(adapter)
+val locationList= arrayOf("Delhi", "Mumbai", "Bengaluru", "Hyderabad", "Chennai", "Kolkata", "Pune", "Ahmedabad", "Jaipur", "Surat",
+    "Lucknow", "Kanpur", "Nagpur", "Indore", "Bhopal", "Patna", "Ludhiana", "Agra", "Vadodara")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, locationList)
+         binding.listOfLocation.setAdapter(adapter)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
